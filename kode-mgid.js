a4b4aa298939bed960f87d9bc138ca4989e3d8be
romsahp <59050778+romsahp@users.noreply.github.com>
2020-04-04 02:01:48 +0700

    Add files via upload

---
+++ kode-mgid.js
@@ -0,0 +1,1 @@
+(function () { var script = document.createElement('script'); script.src = "//jsc.mgid.com/b/l/bloggue.web.id.778521.js?t=" + ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : ''); script.async = true; document.body.appendChild(script); })();
